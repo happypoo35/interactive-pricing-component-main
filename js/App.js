9c7fc1e02476862a9ec4f14cab842296b0b1a36b
Alex <happypoo35@gmail.com>
2021-01-26 22:39:48 +0300

    updated slider values

--- js/App.js
+++ js/App.js
@@ -1,33 +1,71 @@
 const slider = document.getElementById("slider");
 const price = document.getElementById("price");
 const toggle = document.getElementById("toggle");
-const pageviews = document.getElementById("pageviews");
+const pageViews = document.getElementById("pageviews");
 
 const sliderFill = document.documentElement;
 
 slider.addEventListener("input", () => {
-  price.innerHTML = `$${
-    toggle.checked
-      ? Math.floor(((slider.value * 12) / 100) * 75) + ".00"
-      : slider.value + ".00"
-  }<span class="period" id="period">${
-    toggle.checked ? "/ year" : "/ month"
-  }</span>`;
+  if (slider.value <= 10) {
+    pageViews.innerText = `${slider.value * 10}K pageviews`;
 
-  pageviews.innerText = `${Math.floor(slider.value * 6.25)}k Pageviews`;
+    price.innerHTML = `$${
+      toggle.checked
+        ? (parseInt(slider.value) + 7) * 12 * 0.75 + ".00"
+        : parseInt(slider.value) + 7 + ".00"
+    }<span class="period" id="period">${
+      toggle.checked ? "/ year" : "/ month"
+    }</span`;
+  } else if (slider.value < 19) {
+    pageViews.innerText = `${
+      (parseInt(slider.value.split("")[1]) + 1) * 100
+    }K pageviews`;
+
+    price.innerHTML = `$${
+      toggle.checked
+        ? Math.floor(slider.value * 1.9) * 12 * 0.75 + ".00"
+        : Math.floor(slider.value * 1.9) + ".00"
+    }<span class="period" id="period">${
+      toggle.checked ? "/ year" : "/ month"
+    }</span`;
+  } else {
+    pageViews.innerText = "1M pageviews";
+
+    price.innerHTML = `$${
+      toggle.checked ? "324.00" : "36.00"
+    }<span class="period" id="period">${
+      toggle.checked ? "/ year" : "/ month"
+    }</span`;
+  }
 
   sliderFill.style.setProperty(
     "--fillwidth",
-    Math.floor((slider.value / 32) * 100) + "%"
+    Math.floor((slider.value / 19) * 100) + "%"
   );
 });
 
 toggle.addEventListener("click", () => {
-  price.innerHTML = `$${
-    toggle.checked
-      ? Math.floor(((slider.value * 12) / 100) * 75) + ".00"
-      : slider.value + ".00"
-  }<span class="period" id="period">${
-    toggle.checked ? "/ year" : "/ month"
-  }</span>`;
+  if (slider.value <= 10) {
+    price.innerHTML = `$${
+      toggle.checked
+        ? (parseInt(slider.value) + 7) * 12 * 0.75 + ".00"
+        : parseInt(slider.value) + 7 + ".00"
+    }<span class="period" id="period">${
+      toggle.checked ? "/ year" : "/ month"
+    }</span`;
+  } else if (slider.value < 19) {
+    price.innerHTML = `$${
+      toggle.checked
+        ? Math.floor(slider.value * 1.9) * 12 * 0.75 + ".00"
+        : Math.floor(slider.value * 1.9) + ".00"
+    }<span class="period" id="period">${
+      toggle.checked ? "/ year" : "/ month"
+    }</span`;
+  } else {
+    price.innerHTML = `$${
+      toggle.checked ? "324.00" : "36.00"
+    }<span class="period" id="period">${
+      toggle.checked ? "/ year" : "/ month"
+    }</span`;
+  }
 });
